e26b7d6af64377e56514de914014e0a1e60770f1
Mitch Herrema <mitchherrema@Mitch-Herremas-MacBook-Pro.local>
2013-01-11 10:49:09 -0500

    blank project

---
+++ IdeaProjects/Converter/src/gvprojects/Converter/converter.java
@@ -0,0 +1,12 @@
+package gvprojects.Converter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mitchherrema
+ * Date: 1/11/13
+ * Time: 9:47 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class converter {
+
+}
