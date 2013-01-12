c8d489c0cb978b63e7b8487d261aedb3aef8737c
Mitch Herrema <mherrema@yahoo.com>
2013-01-12 14:11:57 -0500

    works without letters

--- IdeaProjects/Converter/src/gvprojects/Converter/converter.java
+++ IdeaProjects/Converter/src/gvprojects/Converter/converter.java
@@ -9,4 +9,41 @@ package gvprojects.Converter;
  */
 public class converter {
 
+    public static void Convert(int num, int base1, int base2) {
+        int baseTen = 0;
+        int newBase = 0;
+        int[] powers1 = new int[32];
+        int[] powers2 = new int[32];
+        int[] tenPowers = new int[32];
+        for (int i = 0; i < 32; i++) {
+            powers1[i] = (int) Math.pow(base1, i);
+            powers2[i] = (int) Math.pow(base2, i);
+            tenPowers[i] = (int) Math.pow(10, i);
+        }
+        for (int i = 31; i >= 0; i--) {
+
+            int tenPow = tenPowers[i];
+            if (num / tenPow != 0) {
+                int current = num / tenPow;
+                baseTen += current * powers1[i];
+                num -= tenPow * current;
+            }
+        }
+        System.out.println(baseTen);
+        for (int i = 31; i >= 0; i--) {
+            int tenPow = tenPowers[i];
+            if (baseTen / powers2[i] != 0) {
+                int current = baseTen / powers2[i];
+
+                newBase += current * tenPow;
+                baseTen -= powers2[i] * current;
+            }
+        }
+        System.out.println(newBase);
+    }
+
+    public static void main(String[] args) {
+        Convert(1000, 2, 5);
+    }
+
 }
